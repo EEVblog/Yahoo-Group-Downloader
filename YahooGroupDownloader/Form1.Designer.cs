5e21d2222c22e2f08655e6c5ecafb0a00ad9438a
Dave Jones <dave@eevblog.com>
2016-02-10 12:33:24 +1100

    Initial commit

---
+++ YahooGroupDownloader/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace YahooGroupDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timeout = new System.Windows.Forms.Timer(this.components);
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label3 = new System.Windows.Forms.Label();
+            this.antifreeze = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.AllowWebBrowserDrop = false;
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(2);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(11, 11);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(613, 316);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.WebBrowserShortcutsEnabled = false;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(84, 6);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(532, 7);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox1.Size = new System.Drawing.Size(613, 126);
+            this.listBox1.TabIndex = 3;
+            // 
+            // timeout
+            // 
+            this.timeout.Interval = 5000;
+            this.timeout.Tick += new System.EventHandler(this.timeout_Tick);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(4, 38);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.webBrowser1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(613, 446);
+            this.splitContainer1.SplitterDistance = 316;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Group Name";
+            // 
+            // antifreeze
+            // 
+            this.antifreeze.Enabled = true;
+            this.antifreeze.Interval = 20000;
+            this.antifreeze.Tick += new System.EventHandler(this.antifreeze_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(427, 7);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 26);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(377, 7);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(45, 26);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "10";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(313, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Timeout (s)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 489);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MinimumSize = new System.Drawing.Size(639, 528);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "YahooGroupDownloader";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timeout;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Timer antifreeze;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ YahooGroupDownloader/Form1.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace YahooGroupDownloader
+{
+   
+
+    public partial class Form1 : Form
+    {
+        string DefaultURL = "https://groups.yahoo.com/neo/groups/${GROUP}/conversations/messages";
+        string PostURL = "/${INDEX}";
+        const int BufferSize = 65536;  // 64 Kilobytes
+        string CurrentURL = "";
+        string GroupName = "No name entered.";
+        int Messages = 0;
+        int PostIndex = 1;
+        int Stage = 0;
+        int LogIndex = 0;
+        float Progress;
+        string prev = "";
+
+        Stopwatch ElapsedTimer = new Stopwatch();
+
+        System.IO.StreamWriter File;
+
+        public Form1()
+        {
+            InitializeComponent();
+            prev = textBox2.Text;
+
+            //Setup timers
+            timeout.Interval = Int32.Parse(textBox2.Text) * 1000;
+            antifreeze.Interval = Int32.Parse(textBox2.Text) * 10000;
+        }
+
+        void AddLog(string Log)
+        {
+            listBox1.Items.Add(LogIndex.ToString() + " :\t" + Log);
+
+            int lines = listBox1.Height / listBox1.ItemHeight;
+            while (listBox1.Items.Count > lines)
+                listBox1.Items.RemoveAt(0);
+
+            listBox1.TopIndex = listBox1.Items.Count - 1;
+            LogIndex += 1;
+        }
+
+        void ToURL(string URL)
+        {
+            try
+            {
+                webBrowser1.Stop();
+                webBrowser1.Navigate(new Uri(URL));
+                AddLog("Navigating to\t" + URL);
+            }
+            catch (System.UriFormatException)
+            {
+                AddLog("Cannot navigate to " + URL);
+                return;
+            }
+        }
+
+        void AddPost(string Title, string Author, string Date, string Content)
+        {
+            string output = "";
+
+            output += "<tr style = \"border: 2px solid #000;\">\n";
+            output += "<td colspan=" + '"' + "1" + '"' + " style = \"border: 2px solid #000;\">" + Title + "</td>\n";
+            output += "<td colspan=" + '"' + "1" + '"' + " style = \"border: 2px solid #000;\">" + Author + "</td>\n";
+            output += "<td colspan=" + '"' + "1" + '"' + " style = \"border: 2px solid #000;\">" + Date + "</td>\n";
+            output += "</tr>\n";
+            output += "<tr style = \"border: 2px solid #000;\">\n";
+            output += "<td colspan=" + '"' + "3" + '"' + " style = \"border: 2px solid #000;\">" + Content + "</td>\n";
+            output += "</tr>\n";
+
+            File.Write(output);
+        }
+
+        void ToMessage(int Index)
+        {
+            //Setup the timeout
+            timeout.Stop();
+            timeout.Enabled = false;
+            
+            CurrentURL = DefaultURL.Replace("${GROUP}", GroupName) + PostURL.Replace("${INDEX}", Index.ToString());
+            ToURL(CurrentURL);
+
+            Progress = (float)((float)(PostIndex) / (float)(Messages) * 100.0);
+
+            float elapsed = ElapsedTimer.ElapsedMilliseconds;
+            float avgtime = elapsed / PostIndex;
+
+            int remaining = (int)(avgtime * (Messages - PostIndex));
+            if (remaining < 0)
+                remaining = 0;
+
+            TimeSpan ts = TimeSpan.FromMilliseconds(remaining);
+            string tream = string.Format("{0:D2}h:{1:D2}m:{2:D2}s:{3:D3}ms",
+                        ts.Hours,
+                        ts.Minutes,
+                        ts.Seconds,
+                        ts.Milliseconds);
+
+            ts = TimeSpan.FromMilliseconds(elapsed);
+            string elaps = string.Format("{0:D2}h:{1:D2}m:{2:D2}s:{3:D3}ms",
+                        ts.Hours,
+                        ts.Minutes,
+                        ts.Seconds,
+                        ts.Milliseconds);
+
+            Text = "Complete: " + (Progress).ToString() + "%    Remaining Time: " + tream + "    Elapsed Time: " + elaps;
+            progressBar1.Value = (int)Progress;
+
+            //Setup the timeout
+            timeout.Enabled = true;
+            timeout.Start();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //Only run if the entire page is loaded
+            if (e.Url.AbsolutePath != (sender as WebBrowser).Url.AbsolutePath)
+                return;
+
+            if (webBrowser1.ReadyState == WebBrowserReadyState.Complete)
+            {
+                HtmlElement doc = webBrowser1.Document.Body;
+
+                switch (Stage)
+                {
+                    case 0:
+                        //Get the number of posts
+                        string Temp = doc.Document.GetElementById("loading-msg").InnerText;
+                        string[] stringSeparators = new string[] { "of total ", " messages" };
+                        string[] result = Temp.Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries);
+                        string CountStr = "";
+                        PostIndex = 0;
+
+                        if (result.Length > 1)
+                        {
+                            CountStr = result[1].Replace(",", "");
+                            Messages = Int32.Parse(CountStr);
+                            AddLog("Group has " + Messages.ToString() + " messages.");
+                            Stage = 1;
+                            PostIndex = 1;
+                            ToMessage(PostIndex);
+                        }
+                        break;
+                    case 1:
+                        //Go through all posts
+                        
+                        if (PostIndex <= Messages)
+                        {
+                            //Get data from message
+                            string[] TitleSep = new string[] { "</SPAN>" };
+                            string PostTitle = doc.Document.GetElementById("yg-msg-subject").InnerHtml.Split(TitleSep, StringSplitOptions.RemoveEmptyEntries)[1];
+                            string PostAuthor = "";
+                            string PostDate = "";
+                            string PostContent = "";
+
+                            //Get document paramters
+                            HtmlElement Content              = doc.Document.GetElementById("yg-msg-view");
+                            HtmlElementCollection Collection = Content.GetElementsByTagName("div");
+                            
+                            //Get the date and author
+                            foreach (HtmlElement Item in Collection)
+                            {
+                                string Attribute = Item.GetAttribute("classname");
+                                if ( Attribute == "author fleft fw-600")
+                                {
+                                    PostAuthor = Item.InnerText;
+                                    AddLog("Author found\t" + PostAuthor);
+                                }
+                                else if (Attribute == "tm single")
+                                {
+                                    PostDate = Item.InnerText;
+                                    AddLog("Date found\t" + PostDate);
+                                }
+                                else if (Attribute == "msg-content undoreset")
+                                {
+                                    PostContent = Item.InnerHtml;
+                                    AddLog("Content found\t" + PostContent);
+                                }
+                            }
+
+                            //Output to a html file
+                            AddPost(PostTitle, PostAuthor, PostDate, PostContent);
+
+                            //Goto next message
+                            PostIndex += 1;
+                            ToMessage(PostIndex);
+                        }
+                        else
+                        {
+                            File.WriteLine("</table>");
+
+                            //Setup the timeout
+                            timeout.Stop();
+                            timeout.Enabled = false;
+                            
+                            AddLog("Yahoo group download complete!");
+                            Text = "YahooGroupDownloader";
+
+                            string fullPath = ((System.IO.FileStream)(File.BaseStream)).Name;
+                            AddLog("Posts output to file -> " + fullPath);
+                            AddLog("Path has been added to your clipboard.");
+                            Clipboard.SetText(fullPath, TextDataFormat.Text);
+
+                            ElapsedTimer.Stop();
+                            ElapsedTimer.Reset();
+
+                            File.Close();
+                            Stage = 2;
+                        }
+                        break;
+                    case 2:
+
+                        break;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0)
+            {
+                GroupName = textBox1.Text;
+                CurrentURL = DefaultURL.Replace("${GROUP}", GroupName);
+                Stage = 0;
+
+                try
+                {
+                    if (File != null)
+                        File.Close();
+
+                    File = new System.IO.StreamWriter((GroupName + ".html"), false, Encoding.UTF8, BufferSize);
+                    File.WriteLine("<table style = \"border: 2px solid #000;\">");
+                    ToURL(CurrentURL);
+
+                    ElapsedTimer.Start();
+                }
+                catch (System.UriFormatException)
+                {
+                    ToURL("File is likely in use, please close it.");
+                }
+            }
+        }
+
+        private void timeout_Tick(object sender, EventArgs e)
+        {
+            AddLog("The following post at index " + PostIndex.ToString() + " timed out.");
+
+            //Goto next message
+            PostIndex += 1;
+            ToMessage(PostIndex);
+        }
+
+
+        string lastURL = "";
+        private void antifreeze_Tick(object sender, EventArgs e)
+        {
+            if (webBrowser1.Url != null)
+            {
+                if (Stage != 2)
+                {
+                    string url = webBrowser1.Url.AbsolutePath;
+                    if (url == lastURL)
+                    {
+                        AddLog("Antifreeze kicked in at " + PostIndex.ToString() + " timed out.");
+
+                        //Goto next message
+                        PostIndex += 1;
+                        ToMessage(PostIndex);
+                    }
+                    lastURL = url;
+                }
+            }
+            else
+            {
+                AddLog("To begin enter the group name then click run.");
+            }
+        }
+
+
+        
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // Filter out non-digit text input
+            string txt = textBox2.Text;
+            for (int i = 0; i < txt.Length; i++)
+            {
+                int c = txt[i];
+                if (c < '0' || c > '9')
+                    textBox2.Text = textBox2.Text.Remove(i, 1);
+            }
+
+
+            if (textBox2.Text.Length > 0 && (prev != textBox2.Text))
+            {
+                //Setup timers
+                timeout.Interval = Int32.Parse(textBox2.Text) * 1000;
+                antifreeze.Interval = Int32.Parse(textBox2.Text) * 10000;
+
+                AddLog("Timeout set to " + (timeout.Interval / 1000).ToString() + " seconds");
+                AddLog("Antifreeze set to " + (antifreeze.Interval / 1000).ToString() + " seconds");
+            }
+
+            prev = textBox2.Text;
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(this, new EventArgs());
+            }
+        }
+    }
+}
